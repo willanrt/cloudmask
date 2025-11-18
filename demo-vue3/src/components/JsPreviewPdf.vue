ccb8635fe587fdae33779cd9867bc24442de1171
Dai Yifan <f1241726242@outlook.com>
2025-11-18 08:43:59 +0800

    Add files via upload

---
+++ demo-vue3/src/components/JsPreviewPdf.vue
@@ -0,0 +1,33 @@
+<template>
+    <div id="pdf"></div>
+</template>
+
+<script>
+import jsPreviewPdf from "@js-preview/pdf";
+export default {
+    name: "JsPreviewPdfDemo",
+    data(){
+        return {
+            myPdfPreviewer: null
+        }
+    },
+    mounted() {
+        this.myPdfPreviewer = jsPreviewPdf.init(document.getElementById('pdf'), {
+            onError: (e)=>{
+                console.log('发生错误', e)
+            },
+            onRendered: ()=>{
+                console.log('渲染完成')
+            }
+        });
+        this.myPdfPreviewer.preview('http://static.shanhuxueyuan.com/test.pdf');
+    },
+    beforeUnmount() {
+        this.myPdfPreviewer.destroy();
+    }
+};
+</script>
+
+<style scoped>
+
+</style>
